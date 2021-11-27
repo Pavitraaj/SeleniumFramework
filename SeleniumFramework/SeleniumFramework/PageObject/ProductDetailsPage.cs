01166b2532ea7a2c34e321706c47dd40e5d092a0
Pavitraaj <pavithraaj4@gmail.com>
2021-11-27 19:14:37 +0100

    Added Tests to the solution

---
+++ SeleniumFramework/SeleniumFramework/PageObject/ProductDetailsPage.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Globalization;
+
+namespace SeleniumTask.PageObjects
+{
+    class ProductDetailsPage : BasePage
+    {
+
+        public ProductDetailsPage(IWebDriver driver) : base(driver)
+        {
+            productPriceTxt = driver.FindElement(priceTextLocator);
+        }
+
+        /// <summary>
+        ///  find element by partial id because for price label locater
+        ///  have two different values for the second substring of id if the item in sale
+        /// </summary>
+        private static By priceTextLocator = By.CssSelector("span[id^='priceblock_']");
+        private IWebElement productPriceTxt;
+
+        /// <summary>
+        /// Return the selected item Price label value in the search list result
+        /// </summary>
+        /// <returns></returns>
+        public decimal GetProductPrice()
+        {
+            string priceText = productPriceTxt.GetAttribute("innerHTML");
+            decimal priceInDecimal = 0;
+            Decimal.TryParse(priceText,
+               NumberStyles.Currency,
+               CultureInfo.CurrentCulture,
+               out priceInDecimal);
+            return priceInDecimal;
+        }
+
+    }
+}
---
+++ SeleniumFramework/SeleniumFramework/Tests/SearchForProductTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using SeleniumTask.PageObjects;
+
+namespace SeleniumTask.Tests
+
+{
+    [TestFixture]
+    class SearchForProductTest : BaseTest
+    {
+        /// <summary>
+        /// Parametrized test to verify the search functionality for any item in amazon by the passed keyword ${searchStringKeyWords}
+        /// Then verify the first Item price in the search result list is more than defined amount price ${price}
+        /// Please note I used  the best practice test naming convention "UnitOfWork_StateUnderTest_ExpectedBehavior"
+        /// </summary>
+        /// <param name="searchStringKeyWords"></param>
+        /// <param name="price"></param>
+        [TestCase("laptop", 100), Description("Verify first Laptop price in the search result is greater than 100$")]
+        public void SearchForProduct_WhenCustomerClickFirstProductInTheResultList_ThenProductPriceShouldBeGreaterThanDefinedValue(string searchStringKeyWords, decimal price)
+        {
+            HomePage homePage = new HomePage(driver);
+            homePage.EnterSearchKeyWord(searchStringKeyWords);
+            SearchResultPage searchResultPage = homePage.ClickOnSearchBtn();
+            ProductDetailsPage itemDetailsPage = searchResultPage.ClickOnShowProductDetailsbtn();
+
+            //Page load timeout worth to be added in a configuration file
+            itemDetailsPage.PageLoadComplete(10000);
+            Assert.Greater(itemDetailsPage.GetProductPrice(), price);
+        }
+    }
+}
